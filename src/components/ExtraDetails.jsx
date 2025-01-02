c42753b6dba7fb305c1b506723fc58b0e7bcd3aa
Maobugichi <immansamuel846@gmail.com>
2025-01-02 09:02:52 -0800

    edit extra section

--- src/components/ExtraDetails.jsx
+++ src/components/ExtraDetails.jsx
@@ -54,7 +54,7 @@ const ExtraDetails = ({quantity,setQuantity,src,setShowCart,name,color,size,pric
     return(
     <section className="h-auto bg-red-300 min-h-[130vh] w-[95%] mx-auto">
             <div className="h-full bg-green-300 flex lg:flex-row flex-col w-full gap-4 justify-center">
-                <div className="w-[80px]  h-[30%]  lg:flex lg:flex-col lg:gap-2 hidden">
+                <div className="w-[80px]   h-[30%]  lg:flex lg:flex-col lg:gap-2 hidden">
                     <div className="rounded-md h-1/2 border border-black grid place-content-center">
                       <img className="w-16 h-20 rounded-sm" src={src} alt={name} />
                     </div>
@@ -63,7 +63,7 @@ const ExtraDetails = ({quantity,setQuantity,src,setShowCart,name,color,size,pric
                       <img className="w-16 h-20 rounded-sm" src={src} alt={name} />
                     </div>
                 </div>
-                <div className="lg:w-[40%] w-full grid place-items-center  h-[70%] ">   
+                <div className="lg:w-[40%] bg-purple-500 w-full grid place-items-center  h-[70%] ">   
                  <img className="w-[90%] lg:h-[600px] h-1/2"  src={src} alt={name} />
                 </div>
                
