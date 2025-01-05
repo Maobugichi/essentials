518c966b453d00b89b0613964181ae33df01abfa
Maobugichi <immansamuel846@gmail.com>
2025-01-05 00:10:51 -0800

    change from browser router to hash router

--- src/main.jsx
+++ src/main.jsx
@@ -1,56 +1,40 @@
+
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import "./index.css";
 import Homepage from "./components/Homepage";
 import Essentials from "./components/Essentials";
 import ExtraSection from "./components/ExtraSection";
 import AuthProfile from "./components/AuthProfile";
 import WishList from "./components/WishList";
-import {Auth0Provider} from "@auth0/auth0-react"
-
+import { Auth0Provider } from "@auth0/auth0-react";
+import { Navigate } from "react-router-dom";
 const redirectUri = `${location.origin}${location.pathname}`;
 const domain = import.meta.env.VITE_AUTH0_DOMAIN?.trim();
 const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID?.trim();
-const router = createBrowserRouter([
-  {
-    path: "/essentials",
-    element: <Homepage/>,
-  },
-  {
-    path: "/essentials/essentialSection/:essentialId",
-    element: <Essentials/>,
-  },
-  {
-    path: "/essentials/extraDetails/:extraDetailId",
-    element: <ExtraSection/>,
-  },
-  {
-    path: "logins/:loginId",
-    element: <AuthProfile/>,
-  },
-  {
-    path: "wishlists/:wishlistId",
-    element: <WishList/>,
-  },
-]);
 
-console.log(clientId)
 ReactDOM.createRoot(document.getElementById("root")).render(
-
   <React.StrictMode>
     <Auth0Provider
-     domain={domain}
-     clientId={clientId}
-     authorizationParams={{
-      redirect_uri:redirectUri
-     }}
+      domain={domain}
+      clientId={clientId}
+      authorizationParams={{
+        redirect_uri: redirectUri,
+      }}
     >
-    <RouterProvider router={router} basename="/essentials"/>
+      <HashRouter>
+        <Routes>
+        <Route path="/" element={<Navigate to="/essentials" replace />} />
+          <Route path="/essentials" element={<Homepage />} />
+          <Route path="/essentials/essentialSection/:essentialId" element={<Essentials />} />
+          <Route path="/essentials/extraDetails/:extraDetailId" element={<ExtraSection />} />
+          <Route path="logins/:loginId" element={<AuthProfile />} />
+          <Route path="wishlists/:wishlistId" element={<WishList />} />
+        </Routes>
+      </HashRouter>
     </Auth0Provider>
-   
   </React.StrictMode>
 );
+
+
